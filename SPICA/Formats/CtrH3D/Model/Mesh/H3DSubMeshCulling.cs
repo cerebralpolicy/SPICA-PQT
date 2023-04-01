4636f177474485fa0ea4f9a574c8f763a988fa65
KillzXGaming <millernathan35@gmail.com>
2023-03-31 21:36:14 -0400

    Update H3DSubMeshCulling.cs

--- SPICA/Formats/CtrH3D/Model/Mesh/H3DSubMeshCulling.cs
+++ SPICA/Formats/CtrH3D/Model/Mesh/H3DSubMeshCulling.cs
@@ -57,17 +57,38 @@ namespace SPICA.Formats.CtrH3D.Model.Mesh
 
         [Ignore]
         public ushort[] Indices;
+        public int MaxIndex
+        {
+            get
+            {
+                int Max = 0;
+
+                foreach (ushort Index in Indices)
+                {
+                    if (Max < Index)
+                        Max = Index;
+                }
+
+                return Max;
+            }
+        }
 
         void ICustomSerialization.Deserialize(BinaryDeserializer Deserializer)
         {
             long Position = Deserializer.BaseStream.Position;
+            bool Is16BitsIdx = (BufferAddress >> 31) != 0;
 
             Indices = new ushort[BufferCount];
 
             Deserializer.BaseStream.Seek(BufferAddress & 0x7fffffff, SeekOrigin.Begin);
 
             for (int Index = 0; Index < BufferCount; Index++)
-                Indices[Index] = Deserializer.Reader.ReadUInt16();
+            {
+                if (Is16BitsIdx)
+                    Indices[Index] = Deserializer.Reader.ReadUInt16();
+                else
+                    Indices[Index] = Deserializer.Reader.ReadByte();
+            }
 
             Deserializer.BaseStream.Seek(Position, SeekOrigin.Begin);
         }
@@ -76,15 +97,35 @@ namespace SPICA.Formats.CtrH3D.Model.Mesh
         {
             BufferCount = (uint)Indices.Length;
 
+            H3DSection Section = H3DSection.RawDataIndex16;
+            object Data;
+
+            if (MaxIndex <= byte.MaxValue)
+            {
+                Section = H3DSection.RawDataIndex8;
+
+                byte[] Buffer = new byte[Indices.Length];
+
+                for (int Index = 0; Index < Indices.Length; Index++)
+                {
+                    Buffer[Index] = (byte)Indices[Index];
+                }
+
+                Data = Buffer;
+            }
+            else
+            {
+                Data = Indices;
+            }
+
             long Position = Serializer.BaseStream.Position;
 
-            H3DSection Section = H3DSection.RawDataIndex16;
             H3DRelocator.AddCmdReloc(Serializer, Section, Position);
 
             Serializer.Sections[(uint)H3DSectionId.RawData].Values.Add(new RefValue()
             {
                 Parent = this,
-                Value = Indices,
+                Value = Data,
                 Position = Position,
                 Padding = 0x10,
             });
