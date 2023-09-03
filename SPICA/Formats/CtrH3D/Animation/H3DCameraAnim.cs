e06c34157f34f97df2adb5f66555f98ffb80ec39
KillzXGaming <millernathan35@gmail.com>
2023-09-03 19:41:33 -0400

    Auto generate light and camera animation element indices

--- SPICA/Formats/CtrH3D/Animation/H3DCameraAnim.cs
+++ SPICA/Formats/CtrH3D/Animation/H3DCameraAnim.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using SPICA.Formats.CtrH3D.Camera;
+using SPICA.Serialization;
+using SPICA.Serialization.Attributes;
+using System.Collections.Generic;
 
 namespace SPICA.Formats.CtrH3D.Animation
 {
-    public class H3DCameraAnim : H3DAnimation
+    public class H3DCameraAnim : H3DAnimation, ICustomSerialization
     {
-        public uint Unknown1;
-        public uint Unknown2;
-        public uint Unknown3;
+        public H3DCameraViewType ViewType;
+        public H3DCameraProjectionType ProjectionType;
+
+        [Inline, FixedLength(10), Padding(4)]
+        public sbyte[] ElementIndices;
 
         public H3DCameraAnim()
         {
+            ElementIndices = new sbyte[10];
         }
 
         public H3DCameraAnim(H3DAnimation Anim) : this()
@@ -26,6 +32,30 @@ namespace SPICA.Formats.CtrH3D.Animation
             Elements.AddRange(Anim.Elements);
 
             MetaData = Anim.MetaData;
+        }
+
+        public void Deserialize(BinaryDeserializer Deserializer)
+        {
+        }
+
+        bool ICustomSerialization.Serialize(BinarySerializer Serializer)
+        {
+            ElementIndices = CalculateElementIndices();
+            return false;
+        }
+
+        private sbyte[] CalculateElementIndices()
+        {
+            sbyte[] indices = new sbyte[10];
+            for (int i = 0; i < indices.Length; i++)
+                indices[i] = -1;
+
+            for (int i = 0; i < this.Elements.Count; i++)
+            {
+                var type = (int)(this.Elements[i].TargetType - H3DTargetType.CameraTransform);
+                indices[type] = (sbyte)i;
+            }
+            return indices;
         }
     }
 }
--- SPICA/Formats/CtrH3D/Animation/H3DLightAnim.cs
+++ SPICA/Formats/CtrH3D/Animation/H3DLightAnim.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using SPICA.Formats.CtrH3D.Camera;
+using SPICA.Formats.CtrH3D.Light;
+using SPICA.Serialization;
+using SPICA.Serialization.Attributes;
+using System.Collections.Generic;
 
 namespace SPICA.Formats.CtrH3D.Animation
 {
-    public class H3DLightAnim : H3DAnimation
+    public class H3DLightAnim : H3DAnimation, ICustomSerialization
     {
-        public uint Unknown1;
-        public uint Unknown2;
-        public uint Unknown3;
-        public uint Unknown4;
+        public H3DLightType LightType;
+
+        [Inline, IfVersion(CmpOp.Greater, 0x21), FixedLength(12), Padding(4)]
+        public sbyte[] ElementIndicesV2;
+
+        [Inline, IfVersion(CmpOp.Lequal, 0x21), FixedLength(9), Padding(4)]
+        public sbyte[] ElementIndicesV1;
 
         public H3DLightAnim()
         {
@@ -27,6 +34,54 @@ namespace SPICA.Formats.CtrH3D.Animation
             Elements.AddRange(Anim.Elements);
 
             MetaData = Anim.MetaData;
+        }
+
+        public void Deserialize(BinaryDeserializer Deserializer)
+        {
+        }
+
+        bool ICustomSerialization.Serialize(BinarySerializer Serializer)
+        {
+            if (Serializer.FileVersion > 0x21)
+                ElementIndicesV2 = CalculateElementIndicesV2();
+            else
+                ElementIndicesV1 = CalculateElementIndicesV1();
+
+            return false;
+        }
+
+        private sbyte[] CalculateElementIndicesV1()
+        {
+            sbyte[] indices = new sbyte[10];
+            for (int i = 0; i < indices.Length; i++)
+                indices[i] = -1;
+
+            for (int i = 0; i < this.Elements.Count; i++)
+            {
+                var target = this.Elements[i].TargetType;
+                //Version does not have these enums, shift downwards
+                if (target >= H3DTargetType.LightGround) target -= 1;
+                if (target >= H3DTargetType.LightSky) target -= 1;
+                if (target >= H3DTargetType.LightInterpolationFactor) target -= 1;
+
+                var type = (int)(target - H3DTargetType.LightTransform);
+                indices[type] = (sbyte)i;
+            }
+            return indices;
+        }
+
+        private sbyte[] CalculateElementIndicesV2()
+        {
+            sbyte[] indices = new sbyte[10];
+            for (int i = 0; i < indices.Length; i++)
+                indices[i] = -1;
+
+            for (int i = 0; i < this.Elements.Count; i++)
+            {
+                var type = (int)(this.Elements[i].TargetType - H3DTargetType.LightTransform);
+                indices[type] = (sbyte)i;
+            }
+            return indices;
         }
     }
 }
